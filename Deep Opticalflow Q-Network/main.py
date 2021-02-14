3b5c227d6961fb47d6abb63ef3edd10972f32a94
otny <40865130+otny@users.noreply.github.com>
2021-02-14 21:24:33 +0900

    Add main.py

---
+++ Deep Opticalflow Q-Network/main.py
@@ -0,0 +1,66 @@
+from src.dqn_agent import DQNAgent
+from src.drqn_agent import DRQNAgent
+from src.dqn_opticalflow_agent import DQNOPTICALFLOWAgent
+from src.config import RetroConfig, GymConfig
+import sys
+
+import argparse
+
+class Main():
+
+    def __init__(self, net_type, conf):
+        if net_type == "drqn":
+            self.agent = DRQNAgent(conf)
+        elif net_type == "dqn_opticalflow":
+            self.agent = DQNOPTICALFLOWAgent(conf)
+        else:
+            self.agent = DQNAgent(conf)
+
+    def train(self, steps):
+        self.agent.train(steps)
+
+    def play(self, episodes, net_path):
+        self.agent.play(episodes, net_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="DRQN")
+    parser.add_argument("--gym", type=str, default="gym", help="Type of the environment. Can either be 'gym' or 'retro'")
+    parser.add_argument("--network_type", type=str, default="dqn", help="Type of the network to build, can either be 'dqn' or 'drqn'")
+    parser.add_argument("--env_name", type=str, default="Breakout-v0", help="Name of the gym/retro environment used to train the agent")
+    parser.add_argument("--retro_state", type=str, default="Start", help="Name of the state (level) to start training. This is only necessary for retro envs")
+    parser.add_argument("--train", type=str, default="True", help="Whether to train a network or to play with a given network")
+    parser.add_argument("--model_dir", type=str, default="saved_session/net/", help="directory to save the model and replay memory during training")
+    parser.add_argument("--net_path", type=str, default="", help="path to checkpoint of model")
+    parser.add_argument("--steps", type=int, default=50000000, help="number of frames to train")
+    parser.add_argument("--flickering", type=bool, default=True, help="flickering play")
+    parser.add_argument("--flickering_p", type=float, default=0.5, help="flickering rate")
+    parser.add_argument("--epsilon_wariai", type=float, default=0.7, help="last epsilon step rate of all steps")
+    args, remaining = parser.parse_known_args()
+
+    if args.gym == "gym":
+        conf = GymConfig()
+        conf.env_name = args.env_name
+    else:
+        conf = RetroConfig()
+        conf.env_name = args.env_name
+        conf.state = args.retro_state
+    conf.network_type = args.network_type
+    conf.train = args.train
+    conf.dir_save = args.model_dir
+    conf.train_steps = args.steps
+    conf.epsilon_decay_episodes = args.steps*conf.epsilon_end_wariai
+    conf.epsilon_decay = float((conf.epsilon_start - conf.epsilon_end))/float(conf.epsilon_decay_episodes)
+    conf.flickering = args.flickering
+    conf.flickering_p = args.flickering_p
+    main = Main(conf.network_type, conf)
+
+    if conf.train == "True":
+        print(conf.train)
+        main.train(conf.train_steps)
+    else:
+        assert args.net_path != "", "Please specify a net_path using the option --net_path"
+        main.play(100000, args.net_path)
+
+
+
